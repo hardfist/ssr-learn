6ea98fab11e7c1b3f2959ea6b013e0f604ec561f
hardfist <1562502418@qq.com>
2018-10-27 22:32:12 +0800

    setup basic ssr
    Change-Id: Ifd5cd7d7790a1b99278c8e9129549ece5a0d2146

---
+++ client/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default class App extends React.Component {
+  render() {
+    return <div>welcome to ssr world</div>;
+  }
+}
--- server/app.js
+++ server/app.js
@@ -1,8 +1,2 @@
-const Koa = require('koa');
-const app = new Koa();
-app.use(async(ctx,next) => {
-  ctx.body = 'hello world'
-})
-app.listen(3333, () => {
-  console.log('listen at 3333')
-});
+import { startServer } from "./server";
+startServer();
